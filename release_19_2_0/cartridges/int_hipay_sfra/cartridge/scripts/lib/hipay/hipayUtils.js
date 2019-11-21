2a26b127cb9b14f44fc71d1d9ed841e800bda479
fburgevin <fburgevin@hipay.com>
2019-11-21 10:26:02 +0100

    Add shipping name indicator

--- release_19_2_0/cartridges/int_hipay_sfra/cartridge/scripts/lib/hipay/hipayUtils.js
+++ release_19_2_0/cartridges/int_hipay_sfra/cartridge/scripts/lib/hipay/hipayUtils.js
@@ -1,19 +1,50 @@
-function normalizeString(str){
-    // Replace - and ' by spaces and uppercase
-    if(typeof str === 'string'){
-        return str
-            .replace(/-/g, ' ')
-            .replace(/'/g, ' ')
-            .toUpperCase();
-    } else {
-        return str;
-    }
+function removeAccents(str) {
+    var r = str.toLowerCase();
+    r = r.replace(new RegExp("[àáâãäå]", 'g'),"a");
+    r = r.replace(new RegExp("æ", 'g'),"ae");
+    r = r.replace(new RegExp("ç", 'g'),"c");
+    r = r.replace(new RegExp("[èéêë]", 'g'),"e");
+    r = r.replace(new RegExp("[ìíîï]", 'g'),"i");
+    r = r.replace(new RegExp("ñ", 'g'),"n");
+    r = r.replace(new RegExp("[òóôõö]", 'g'),"o");
+    r = r.replace(new RegExp("œ", 'g'),"oe");
+    r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
+    r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
+    return r;
 }
 
-function compareStrings(str1, str2){
+function normalizeString(str) {
+    if(typeof str !== 'string'){
+        return str;
+    }
+
+    var normalizedStr;
+
+    normalizedStr = removeAccents(str);
+    normalizedStr = normalizedStr
+        .trim()
+        .replace(/-/g, ' ')
+        .replace(/'/g, ' ')
+        .toUpperCase();
+
+    return normalizedStr;
+}
+
+function compareStrings(str1, str2) {
     return normalizeString(str1) === normalizeString(str2);
 }
 
+function compareNames(firstname1, lastname1, firstname2, lastname2) {
+    // Normalize strings
+    var name1 = normalizeString(firstname1) + ' ' + normalizeString(lastname1);
+    var name2 = normalizeString(firstname2) + ' ' + normalizeString(lastname2);
+    var inversedName2 = normalizeString(lastname2) + ' ' + normalizeString(firstname2);
+
+    // Same if name1 equals name2 or inversed firstnames and lastnames
+    return (name1 === name2) || (name1 === inversedName2);
+}
+
 module.exports = {
-    compareStrings: compareStrings
+    compareStrings: compareStrings,
+    compareNames: compareNames
 };

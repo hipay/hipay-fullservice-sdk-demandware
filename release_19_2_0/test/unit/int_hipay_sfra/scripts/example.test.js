6f15d34e472334d81b5ad384c346c3716fa77f59
fburgevin <fburgevin@hipay.com>
2019-11-26 14:35:00 +0100

    Add example unit test

---
+++ release_19_2_0/test/unit/int_hipay_sfra/scripts/example.test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('test', function () {
+    describe('test', function () {
+        it('Should return true', function() {
+            assert.isTrue(true);
+        })
+    });
+});
